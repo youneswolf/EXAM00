17597ec8cfbc74d7c077fa7f5d70de5e047a00a4
youneswolf <younesbellakrid@gmail.com>
2023-05-04 10:44:26 +0100

    str_cmpr

---
+++ ex08.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0'){
+        if(s1[i]==s2[2]){
+         i++;
+}else
+	return (s1[i] - s2[i]);
+}
+}
